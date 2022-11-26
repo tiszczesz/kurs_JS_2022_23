36edc127a3f371e6bee66c46d2885619027ac346
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2022-11-26 11:58:07 +0100

    ff

--- Typescript_exercises/cw5/src/cw5.ts
+++ Typescript_exercises/cw5/src/cw5.ts
@@ -1,3 +1,7 @@
 import { Films } from "./dane.js";
 
 console.log(Films);
+console.log(Films[0]);
+console.log(Films[0].release_date);
+console.log(Films[0].opening_crawl);
+
--- Typescript_exercises/cw5/src/dane.ts
+++ Typescript_exercises/cw5/src/dane.ts
@@ -1,4 +1,14 @@
-export const Films:Array<any> =  await GetFilms();
+export const Films:Array<FilmInfo> =  await GetFilms();
+
+export interface FilmInfo{
+    title:string,
+    opening_crawl:string,
+    created:string,
+    director:string,
+    edited:string,
+    episode_id:number,
+    release_date:Date
+}
 
 async function GetFilms() {
     const res = await fetch('https://swapi.dev/api/films/');

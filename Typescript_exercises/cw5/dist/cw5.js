6c844504e9b657faa70615eba2fed9f2a72b9376
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2022-11-26 12:29:28 +0100

    gg

--- Typescript_exercises/cw5/dist/cw5.js
+++ Typescript_exercises/cw5/dist/cw5.js
@@ -3,3 +3,27 @@ console.log(Films);
 console.log(Films[0]);
 console.log(Films[0].release_date);
 console.log(Films[0].opening_crawl);
+const root = document.querySelector("#root");
+if (root != null) {
+    root.innerHTML = getAll(Films);
+}
+function getAll(Films) {
+    let html = "<div class='myFlex'>";
+    Films.forEach((e, i) => {
+        html += generCard(e);
+    });
+    return html + "</div>";
+}
+function generCard(film) {
+    let html = `<div class="card"  >
+    <div class="card-body">
+      <h5 class="card-title" style='{color:green;font-weight:bold}'>${film.title}</h5>
+      <h6 class="card-subtitle mb-2 text-muted">${film.director}</h6>
+      <p class="card-text">${film.opening_crawl}</p>
+      <div class="card-footer text-muted">
+            Date: ${film.release_date}
+     </div>
+    </div>
+  </div>`;
+    return html;
+}
--- Typescript_exercises/cw5/src/cw5.ts
+++ Typescript_exercises/cw5/src/cw5.ts
@@ -1,7 +1,32 @@
-import { Films } from "./dane.js";
+import { Films, FilmInfo } from "./dane.js";
 
 console.log(Films);
 console.log(Films[0]);
 console.log(Films[0].release_date);
 console.log(Films[0].opening_crawl);
+const root = document.querySelector("#root");
+if(root !=null){
+    root.innerHTML = getAll(Films);
+}
 
+function getAll(Films:Array<FilmInfo>):string{
+    let html = "<div class='myFlex'>";
+    Films.forEach((e,i)=>{
+        html += generCard(e);
+    });
+    return html + "</div>";
+}
+
+function generCard(film: FilmInfo): string {
+    let html = `<div class="card"  >
+    <div class="card-body">
+      <h5 class="card-title" style='{color:green;font-weight:bold}'>${film.title}</h5>
+      <h6 class="card-subtitle mb-2 text-muted">${film.director}</h6>
+      <p class="card-text">${film.opening_crawl}</p>
+      <div class="card-footer text-muted">
+            Date: ${film.release_date}
+     </div>
+    </div>
+  </div>`;
+    return html;
+}

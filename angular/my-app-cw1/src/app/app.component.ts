3bf8f9ffb68b1acbaa896ea8f9de572bbd0cf793
tiszczesz <tiszczesz@gmail.com>
2022-12-10 12:40:18 +0100

    gg

--- angular/my-app-cw1/src/app/app.component.ts
+++ angular/my-app-cw1/src/app/app.component.ts
@@ -7,5 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Pierwsza aplikacja Angular';
-  
+  users = [
+    {firstname:"Adam",lastname:"Nowak",klasa:"1TIP"},
+    {firstname:"Monika",lastname:"Nowacka",klasa:"3TIP"},
+    {firstname:"Grażyna",lastname:"Nowak",klasa:"1B"},
+    {firstname:"Ryszard",lastname:"Nowak",klasa:"1C"},
+    {firstname:"Tomasz",lastname:"Gocki",klasa:"3TIP"},
+    {firstname:"Roman",lastname:"Kowalski",klasa:"4TIP"},
+    {firstname:"Adrian",lastname:"Słomiński",klasa:"2TIP"},
+    {firstname:"Adrian",lastname:"Słomiński",klasa:"2TIP"},
+    {firstname:"Adrian",lastname:"Słomiński",klasa:"2TIP"},
+  ]
 }

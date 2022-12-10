c980debb978e1d5c0e770bd49bc5188749c752f4
tiszczesz <tiszczesz@gmail.com>
2022-12-10 13:46:45 +0100

    hh

---
+++ angular/cw2-app/src/app/app.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { Game } from './Game';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  addGame() {
+    
+       this.games.push(new Game(this.gameTitle,this.gamePrice,this.gameCategory));
+   
+      
+   
+   
+  }
+  onInputTitle(val: string) {
+    this.gameTitle = val;
+    console.log(val);
+   
+  }
+  onInputPrice(val: string) {
+    this.gamePrice = parseFloat(val);
+    console.log(val);
+  
+  }
+  onInputCategory(val: string) {
+    this.gameCategory = val;
+    console.log(val);
+   
+  }
+  title = 'cw2-app';
+  currentDate = new Date().toLocaleDateString();
+  //inputGame: Game = new Game("ddd");
+  gameTitle:string = ''
+  gamePrice:number = 0
+  gameCategory:string = ''
+
+  games: Game[] = [
+    new Game("Superman 3", 69.99),
+    new Game("FIFA 2022", 169.99),
+    new Game("Diablo 4", 350),
+    new Game("Diablo 55", 350),
+  ];
+}

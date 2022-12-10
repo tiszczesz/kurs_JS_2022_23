f46fc19825611a12f9124a1dace2cdb30f986584
tiszczesz <tiszczesz@gmail.com>
2022-12-10 12:32:14 +0100

    gg

--- angular/my-app-cw1/src/app/app.module.ts
+++ angular/my-app-cw1/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyFirstComponent } from './my-first/my-first.component';
+import { MyCardComponent } from './my-card/my-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyFirstComponent,
+    MyCardComponent
   ],
   imports: [
     BrowserModule,
